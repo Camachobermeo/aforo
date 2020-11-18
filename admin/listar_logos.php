9e698174b1be5e50bef6bfcb7cd108d9d78bede2
Camachobermeo <joelbermeo452@gmail.com>
2020-11-18 09:03:11 -0500

    lista de logo

---
+++ admin/listar_logos.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+<?php
+
+// if (!isset($_POST["archivo"])) {
+//     exit();
+// }
+
+include_once "../utiles/base_de_datos.php";
+$url = "utiles/videos/video1.mp4";
+try {
+$sentencia = $base_de_datos->prepare("INSERT INTO logo(url_logo) VALUES (?);");
+$resultado = $sentencia->execute ([$url]);
+
+ if ($resultado === true) {
+    header("Location: tabla_logo.php?guardado=1");
+ } else {
+    header("Location: logo.php?fallo=1");
+ }
+} catch (\Throwable $th) {
+    header("Location: logo.php?fallo=1");
+}
