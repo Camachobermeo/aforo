7f65ae11bf660475760ded0e5173d71bd9e220b6
Mario Mario <marioramos083@gmail.com>
2020-11-23 22:03:26 -0500

    123

--- admin/video/eliminarvideo.php
+++ admin/video/eliminarvideo.php
@@ -1,8 +1,8 @@
 <?php
 
 if (
-!isset($_GET["id"])||
-!isset($_GET["nombre"])
+    !isset($_GET["id"]) ||
+    !isset($_GET["nombre"])
 ) {
     exit();
 }
@@ -12,7 +12,7 @@ include_once "../../utiles/base_de_datos.php";
 $sentencia = $base_de_datos->prepare("DELETE FROM video WHERE id_video = ?;");
 $resultado = $sentencia->execute([$id]);
 if ($resultado === true) {
-    unlink("../../utiles/videos/".$nombre);
+    unlink("../../utiles/videos/" . $nombre);
     header("Location: tablavideo.php?guardado=1");
 } else {
     echo "Algo salió mal";
