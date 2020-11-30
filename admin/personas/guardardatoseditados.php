60a19228b0fd99875f95df03a2f142be6eaf2d23
Mario Mario <marioramos083@gmail.com>
2020-11-30 11:33:02 -0500

    arreglar

--- admin/personas/guardardatoseditados.php
+++ admin/personas/guardardatoseditados.php
@@ -4,44 +4,44 @@
 
 <?php
 
+
+if (
+    !isset($_POST["nombre"]) ||
+    !isset($_POST["apellidos"]) ||
+    !isset($_POST["rut"]) ||
+    !isset($_POST["rfid"])
+) {
+    exit();
+}
+
+include_once "../../utiles/base_de_datos.php";
+$id = $_POST["id"];
+$nombre = $_POST["nombre"];
+$apellidos = $_POST["apellidos"];
+$rut = $_POST["rut"];
+$rfid = $_POST["rfid"];
+
 if (isset($_POST['subir'])) {
     $archivo = $_FILES['archivo']['name'];
-    if (isset($archivo) && $archivo != "") {
-        $tamano = $_FILES['archivo']['size'];
-        $temp = $_FILES['archivo']['tmp_name'];
-        if (move_uploaded_file($temp, '../../utiles/persona/' . $archivo)) {
-            chmod('../../utiles/persona/' . $archivo, 0777);
+}
+$url = $archivo;
+try {
 
-            if (
-                !isset($_POST["nombre"]) ||
-                !isset($_POST["apellidos"]) ||
-                !isset($_POST["rut"]) ||
-                !isset($_POST["rfid"])
-            ) {
-                exit();
-            }
+    $sentencia = $base_de_datos->prepare("UPDATE persona SET nombre = ?, apellidos = ?, rut = ?, rfid = ?, url_foto = ? WHERE id_persona = ?;");
+    $resultado = $sentencia->execute([strtoupper($nombre), strtoupper($apellidos), strtoupper($rut), strtoupper($rfid), strtoupper($url), $id]);
 
-
-            include_once "../../utiles/base_de_datos.php";
-            $id = $_POST["id"];
-            $nombre = $_POST["nombre"];
-            $apellidos = $_POST["apellidos"];
-            $rut = $_POST["rut"];
-            $rfid = $_POST["rfid"];
-            $url = $archivo;
-            try {
-
-                $sentencia = $base_de_datos->prepare("UPDATE persona SET nombre = ?, apellidos = ?, rut = ?, rfid = ?, url_foto = ? WHERE id_persona = ?;");
-                $resultado = $sentencia->execute([strtoupper($nombre), strtoupper($apellidos), strtoupper($rut), strtoupper($rfid), strtoupper($url), $id]);
-
-                if ($resultado === true) {
-                    header("Location: listarpersona.php?guardado=1");
-                } else {
-                    header("Location: editarpersona.php?id=$id&&daniado=1");
-                }
-            } catch (\Throwable $th) {
-                header("Location: editarpersona.php?id=$id&&daniado=1");
+    if ($resultado === true) {
+        if (isset($archivo) && $archivo != "") {
+            $tamano = $_FILES['archivo']['size'];
+            $temp = $_FILES['archivo']['tmp_name'];
+            if (move_uploaded_file($temp, '../../utiles/persona/' . $archivo)) {
+                chmod('../../utiles/persona/' . $archivo, 0777);
             }
         }
+        header("Location: listarpersona.php?guardado=1");
+    } else {
+        header("Location: editarpersona.php?id=$id&&daniado=1");
     }
+} catch (\Throwable $th) {
+    header("Location: editarpersona.php?id=$id&&daniado=1");
 }
